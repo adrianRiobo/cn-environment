a3206b5e4d987654105e4737acd479c7e13b6f5b
Adrian Riobo <adrian.riobo.lorenzo@everis.com>
2020-01-15 11:21:15 +0000

    Updated credentials configure script based on csv syntax

--- configure_credentials.sh
+++ configure_credentials.sh
@@ -2,5 +2,5 @@
 CREDENTIALS_FILE=/vagrant/credentials.csv
 sudo yum install -y dos2unix 
 dos2unix $CREDENTIALS_FILE
-echo "export AWS_ACCESS_KEY_ID=$(head -1 $CREDENTIALS_FILE | cut -d '=' -f2)" | tee -a /home/vagrant/.bash_profile
-echo "export AWS_SECRET_ACCESS_KEY=$(tail -1 $CREDENTIALS_FILE | cut -d '=' -f2)" | tee -a /home/vagrant/.bash_profile
+echo "export AWS_ACCESS_KEY_ID=$(tail -1 $CREDENTIALS_FILE | cut -d ',' -f1)" | tee -a /home/vagrant/.bash_profile
+echo "export AWS_SECRET_ACCESS_KEY=$(tail -1 $CREDENTIALS_FILE | cut -d ',' -f2)" | tee -a /home/vagrant/.bash_profile
